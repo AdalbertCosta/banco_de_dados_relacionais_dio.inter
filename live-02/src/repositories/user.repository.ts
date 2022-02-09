6a3356190b6a49cf7f3b386c07a90d297bd35b9e
AdalbertCosta <contato@adalbertocosta.com>
2022-02-09 06:45:30 -0300

    Realizando consulta users

--- live-02/src/repositories/user.repository.ts
+++ live-02/src/repositories/user.repository.ts
@@ -1,12 +1,18 @@
-import User from "../models/user.model";
+import db from "../db";
+import User from "../models/user.models";
+
 
 class UserRepository {
 
-    findAllUsers(): User[] {
-      return [];
+    async findAllUsers(): Promise<User[]> {
+        const query = `SELECT uuid, username 
+        FROM application_user
+        `;
+
+        const result = await db.query<User>(query);
+        const rows = result.rows;
+        return rows || [];
     }
-
-
 }
 
 export default new UserRepository();
