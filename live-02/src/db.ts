ffe4be0098a77faa90e21d3f55b07537d929d475
AdalbertCosta <contato@adalbertocosta.com>
2022-02-08 21:22:15 -0300

    Complementando DB

--- live-02/src/db.ts
+++ live-02/src/db.ts
@@ -1,7 +1,8 @@
-import {Poll} from 'pg';
+import {Pool} from 'pg';
 
-const connectionString = '';
-const db = new Pool({connectionString})
+const connectionString = 'postgres://bhgsulqh:4fQZHXf54qKF5FyDrtAD3BIJae7_UERe@tyke.db.elephantsql.com/bhgsulqh';
+
+const db = new Pool ({connectionString});
 
 export default db;
 
