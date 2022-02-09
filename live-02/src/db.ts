3d92235eb2f84f9f4734ff44a1f4257b5a4882f5
AdalbertCosta <contato@adalbertocosta.com>
2022-02-09 07:08:39 -0300

    Alterando link db

--- live-02/src/db.ts
+++ live-02/src/db.ts
@@ -1,8 +1,10 @@
-import {Pool} from 'pg';
+import { Pool } from 'pg';
 
 const connectionString = 'postgres://bhgsulqh:4fQZHXf54qKF5FyDrtAD3BIJae7_UERe@tyke.db.elephantsql.com/bhgsulqh';
 
-const db = new Pool ({connectionString});
+const db = new Pool({ connectionString });
 
 export default db;
+
+
 
