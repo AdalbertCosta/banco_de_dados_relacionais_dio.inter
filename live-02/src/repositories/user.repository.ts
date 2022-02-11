b070f0d375c9913919690580a2c80e4e02739b5b
AdalbertCosta <contato@adalbertocosta.com>
2022-02-11 06:47:13 -0300

    DEsenvolvendo inserção de usuários

--- live-02/src/repositories/user.repository.ts
+++ live-02/src/repositories/user.repository.ts
@@ -1,34 +1,45 @@
-import db from "../db";
-import User from "../models/user.models";
-
+import db from '../db'
+import User from '../models/user.models'
 
 class UserRepository {
-
-    async findAllUsers(): Promise<User[]> {
-        const query = `SELECT uuid, username 
+  async findAllUsers(): Promise<User[]> {
+    const query = `SELECT uuid, username 
         FROM application_user
-        `;
+        `
 
-        const {rows} = await db.query<User>(query);
-        return rows || [];
-    }
+    const { rows } = await db.query<User>(query)
+    return rows || []
+  }
 
-    async findById(uuid: string): Promise<User> {
-        const query = `
+  async findById(uuid: string): Promise<User> {
+    const query = `
                 SELECT uuid, usernames, email
                 FROM application_user
                 WHERE uuid - $1
-                `;
+                `
 
-                const values = {uudi};
+    const values = { uudi }
 
-                const {row} = await db.query<User>(query, values);
-                const [user] = rows;
+    const { row } = await db.query<User>(query, values)
+    const [user] = rows
 
-                return User;
-        
-        
-    }
+    return User
+  }
+
+  async create(user: User): Promise<string> {
+      const script = `
+      INSERT INTO application_user (
+        username,
+        password
+        )
+        VALUES ($1, crypt($2,'my_salt'))
+        RETURNIG uuid
+      `;
+
+      const values = [user.username, user.password];
+      const {rows} = await db.query<{uuid: string}>(script, values);
+      const [newUser] = rows;
+      const newUseer.uuid;
+  }
 }
-
 export default new UserRepository();
--- live-02/src/routes/users.route.ts
+++ live-02/src/routes/users.route.ts
@@ -1,6 +1,6 @@
-import { NextFunction,Request,Response,Router} from "express";
-import {StatusCodes} from 'http-status-codes';
-import userRepository from '../repositories/user.repository';
+import { NextFunction, Request, Response, Router } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import userRepository from '../repositories/user.repository'
 
 // get /users
 // get /users/:uuid
@@ -8,43 +8,49 @@ import userRepository from '../repositories/user.repository';
 // put /users/:uuid
 // delete /users/:uuid
 
+const usersRoute = Router()
 
-const usersRoute = Router();
+usersRoute.get(
+  '/users',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const users = await userRepository.findAllUsers()
+    res.status(StatusCodes.OK).send(users)
+  }
+)
 
-usersRoute.get('/users', async (req: Request, res: Response, next: NextFunction) => {
-    const users = await userRepository.findAllUsers() ;
-    res.status(StatusCodes.OK).send(users);
-});
-
-
-usersRoute.get('/users/:uuid', async (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
-  const uuid = req.params.uuid;
-  const user = await userRepository.findById(uuid);
-  res.status(StatusCodes.OK).send({uuid});
-});
+usersRoute.get(
+  '/users/:uuid',
+  async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid
+    const user = await userRepository.findById(uuid)
+    res.status(StatusCodes.OK).send({ uuid })
+  }
+)
 
 usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
-  const newUser = req.body;
-  console.log(req.body);
-  res.status(StatusCodes.CREATED).send(newUser);
-});
+  const newUser = req.body
 
+  const uuid = await UserRepository.create(newUser)
 
-usersRoute.put('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
-  const uuid = req.params.uuid;
-  const modifiedUser = req.body;
-  modifiedUser.uuid = uuid;
-  res.status(StatusCodes.OK).send({modifiedUser})
-});
+  res.status(StatusCodes.CREATED).send(uuid)
+})
 
-usersRoute.delete('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
-  const uuid = req.params.uuid;
-  res.sendStatus(StatusCodes.OK)
-});
+usersRoute.put(
+  '/users/:uuid',
+  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid
+    const modifiedUser = req.body
+    modifiedUser.uuid = uuid
+    res.status(StatusCodes.OK).send({ modifiedUser })
+  }
+)
 
+usersRoute.delete(
+  '/users/:uuid',
+  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid
+    res.sendStatus(StatusCodes.OK)
+  }
+)
 
-
-
-export default usersRoute;
-
-
+export default usersRoute
